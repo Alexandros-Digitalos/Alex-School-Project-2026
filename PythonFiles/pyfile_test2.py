48c1e3820f6ab835c3d58de9e50aa18b983ac3b3
Alexandros Digitalos <152096497+Alexandros-Digitalos@users.noreply.github.com>
2026-03-11 09:42:27 +0200

    Json stuff

--- PythonFiles/pyfile_test2.py
+++ PythonFiles/pyfile_test2.py
@@ -1,7 +1,11 @@
-import pydivert
+from pydivert import *
+from json import *
 
-with pydivert.WinDivert() as wdiv:
+with WinDivert() as wdiv:
     for packet in wdiv:
         print(packet)
         wdiv.send(packet)
+        json_data = dumps(str(packet), indent=4)
+        captured_packet = open("captured_packet.json", "w", encoding="utf-8") 
+        captured_packet.write(json_data)
         break
